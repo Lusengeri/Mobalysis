c89703a64c0ab0be29dc80499473f0a9ab8416ca
Bethwel <bethwel.korir@azubiafrica.org>
2022-02-12 07:27:30 -0800

    useradd

--- setup.sh
+++ setup.sh
@@ -7,4 +7,7 @@ sudo apt install -y postgresql postgresql-contrib
 
 # Create Postgres User: mob_db_user with password mob_db_password
 sudo -u postgres psql -c "CREATE USER mob_db_user;"
-sudo -u postgres psql -c "ALTER USER mob_db_user password 'mob_db_pass';" 
+sudo -u postgres psql -c "ALTER USER mob_db_user password 'mob_db_pass';"
+#add user
+cd /home
+useradd mob_app_usr 
