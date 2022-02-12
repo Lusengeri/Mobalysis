0c3f5ec8b5574dc8765507b982bedce1a51d0bc6
root <theophilus.bittok@azubiafica.org>
2022-02-12 18:46:07 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -7,4 +7,8 @@ sudo apt install -y postgresql postgresql-contrib
 
 # Create Postgres User: mob_db_user with password mob_db_password
 sudo -u postgres psql -c "CREATE USER mob_db_user;"
-sudo -u postgres psql -c "ALTER USER mob_db_user password 'mob_db_pass';" 
+sudo -u postgres psql -c "ALTER USER mob_db_user password 'mob_db_pass';"
+
+#Task3
+cd /home/mob_app_usr 
+git clone https://github.com/theophilusbittok1/Mobalysis.git 
