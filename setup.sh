8af20f0701305188987a5044ffb1ed90afce4765
root <theophilus.bittok@azubiafica.org>
2022-02-12 19:08:36 +0300

    addedsetup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,13 @@ sudo apt install -y postgresql postgresql-contrib
 sudo -u postgres psql -c "CREATE USER mob_db_user;"
 sudo -u postgres psql -c "ALTER USER mob_db_user password 'mob_db_pass';"
 
+#add user
+cd /home
+useradd mob_app_usr
 
+#
+cd /home/mob_app_usr 
+git clone https://github.com/theophilusbittok1/Mobalysis.git
 
 
 
