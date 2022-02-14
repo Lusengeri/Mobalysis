6adb83d5a871b8ebafcb3e162566a1b7dc85e67f
Bethwel <bethwel.korir@azubiafrica.org>
2022-02-14 07:23:52 -0800

    install virtualenvi

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ export DBUSER="mob_db_user"
 export DBPASS="mob_db_pass" 
 export DBHOST="localhost" 
 export DBPORT="5432"
+
+#Install application packages for backend in virtual environment
+sudo apt-get install python3-pip
+sudo pip3 install virtualenv
+virtualenv env
+
 # Clone Mobalysis repository to home directory
 cd /home/mob_app_usr 
 git clone https://github.com/theophilusbittok1/Mobalysis.git
