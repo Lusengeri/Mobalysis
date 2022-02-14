e3ad17740f7adad36e218f45012f66036898006f
root <theophilus.bittok@azubiafica.org>
2022-02-14 18:11:42 +0300

    DB Parameters

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-DBNAME="mobalytics" 
-DBUSER="mob_db_user" 
-DBPASS="mob_db_pass" 
-DBHOST="localhost" 
-DBPORT="5432"
+
+export DBNAME="mobalytics" 
+export DBUSER="mob_db_user" 
+export DBPASS="mob_db_pass" 
+export DBHOST="localhost" 
+export DBPORT="5432"
 # Clone Mobalysis repository to home directory
 cd /home/mob_app_usr 
 git clone https://github.com/theophilusbittok1/Mobalysis.git
