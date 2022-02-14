661c93796de9f1492b81873197518b823efdb021
Bethwel <bethwel.korir@azubiafrica.org>
2022-02-14 05:55:13 -0800

    db parameters updated

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+DBNAME="mobalytics" 
+DBUSER="mob_db_user" 
+DBPASS="mob_db_pass" 
+DBHOST="localhost" 
+DBPORT="5432"
 # Clone Mobalysis repository to home directory
 cd /home/mob_app_usr 
 git clone https://github.com/theophilusbittok1/Mobalysis.git
