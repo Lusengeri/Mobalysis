1fb5d9cd40019586cc041e3594aaf7934eb1547b
Lusengeri <lusengeri@gmail.com>
2022-02-14 14:00:41 +0300

    setup.sh: 'venv' install, db setup

--- setup.sh
+++ setup.sh
@@ -17,10 +17,11 @@ useradd mob_app_usr
 cd /home/mob_app_usr 
 git clone https://github.com/theophilusbittok1/Mobalysis.git
 
+# Install the 'python3-venv' package
+sudo apt install python3-venv
 
+# Create a postresql database with the name 'mobalytics'
+sudo -u postgres psql -c "CREATE DATABASE mobalytics;"
 
-
-
-
-
-
+# Set the owner of the database to 'mob_db_user'
+sudo -u postgres psql -c "ALTER DATABASE mobalytics OWNER TO mob_db_user;"
