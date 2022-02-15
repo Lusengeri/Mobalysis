690225f89b81a5df01b1f1a0e1ea4423cae244cb
Lusengeri <lusengeri@gmail.com>
2022-02-15 16:44:45 +0300

    install.sh: make and install migrations

--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-
-export DBNAME="mobalytics" 
-export DBUSER="mob_db_user" 
-export DBPASS="mob_db_pass" 
+export DBNAME="mobalytics"
+export DBUSER="mob_db_user"
+export DBPASS="mob_db_pass"
 export DBHOST="localhost" 
 export DBPORT="5432"
+BACKEND_PATH="/home/mob_app_usr/Mobalysis/backend/"
+
+# Clone Mobalysis repository to home directory
+cd /home/mob_app_usr
+git clone https://github.com/theophilusbittok1/Mobalysis.git
 
 #Install application packages for backend in virtual environment
 sudo apt-get install python3-pip
 sudo pip3 install virtualenv
 sudo virtualenv env
 sudo source env/bin/activate
-sudo pip install <libraries>
+sudo pip install -r "${BACKEND_PATH}requirements.txt"
 
-# Clone Mobalysis repository to home directory
-cd /home/mob_app_usr 
-git clone https://github.com/theophilusbittok1/Mobalysis.git
+# Make and install database migrations
+python3 "${BACKEND_PATH}manage.py make migrations"
+python3 "${BACKEND_PATH}manage.py migrate"
