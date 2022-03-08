322086afeb958a739e5f9921956061765c98ddcd
Lusengeri <lusengeri@gmail.com>
2022-03-08 16:47:13 +0300

    Fix setup.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-export DBNAME="mobalytics"
-export DBUSER="mob_db_user"
-export DBPASS="mob_db_pass"
-export DBHOST="localhost" 
-export DBPORT="5432"
-BACKEND_PATH="/home/mob_app_usr/Mobalysis/backend/"
+export APP_USER_HOME=/home/mob_app_usr/
+export BACKEND_PATH="${APP_USER_HOME}Mobalysis/backend/"
+
+sudo echo 'export DBNAME=mobalytics' >> "${APP_USER_HOME}.bashrc"
+sudo echo 'export DBUSER=mob_db_user' >> "${APP_USER_HOME}.bashrc"
+sudo echo 'export DBPASS=mob_db_pass' >> "${APP_USER_HOME}.bashrc"
+sudo echo 'export DBHOST=localhost' >> "${APP_USER_HOME}.bashrc"
+sudo echo 'export DBPORT=5432' >> "${APP_USER_HOME}.bashrc"
 
 # Clone Mobalysis repository to home directory
-cd /home/mob_app_usr
+cd $APP_USER_HOME 
 git clone https://github.com/theophilusbittok1/Mobalysis.git
 
 #Install application packages for backend in virtual environment
-sudo apt-get install python3-pip
-sudo pip3 install virtualenv
-sudo virtualenv env
-sudo source env/bin/activate
+sudo apt install -y python3-pip
+sudo apt install -y python3-venv
+sudo python3 -m venv env
+source env/bin/activate
 sudo pip install -r "${BACKEND_PATH}requirements.txt"
 
 # Make and install database migrations
--- setup.sh
+++ setup.sh
@@ -1,22 +1,52 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Install all dependencies
 sudo apt update
+sudo apt install python3-dev gcc python3-distutils python3-pip python3-venv postgresql nginx -y
 
-# Install postgres chosing 'yes' for all prompts if possible
-sudo apt install -y postgresql postgresql-contrib
+# Set up postgres database
+sudo -u postgres psql -c "create user mob_db_user with encrypted password 'mobdbpass'"
+sudo -u postgres psql -c "create database mobalysis"
+sudo -u postgres psql -c "grant all privileges on database mobalysis to mob_db_user"
 
-# Create Postgres User: mob_db_user with password mob_db_password
-sudo -u postgres psql -c "CREATE USER mob_db_user;"
-sudo -u postgres psql -c "ALTER USER mob_db_user password 'mob_db_pass';"
+#Set up the mob_app_user user
+sudo useradd -m mob_app_user
+sudo chsh -s /sbin/nologin mob_app_user
+sudo -u mob_app_user sh -c 'cd /home/mob_app_user/ && git clone https://github.com/theophilusbittok1/Mobalysis'
 
-# Add user and create home directory for the user
-sudo useradd mob_app_usr -m -d "/home/mob_app_usr"
+#set up the application environment
+sudo  bash -c 'echo "export DBUSER=mob_db_user" >> /home/mob_app_user/.bashrc'
+sudo  bash -c 'echo "export DBNAME=mobalysis" >> /home/mob_app_user/.bashrc'
+sudo  bash -c 'echo "export DBHOST=127.0.0.1" >> /home/mob_app_user/.bashrc'
+sudo  bash -c 'echo "export DBPASS=mobdbpass" >> /home/mob_app_user/.bashrc'
+sudo  bash -c 'echo "export DBPORT=5432" >> /home/mob_app_user/.bashrc'
 
-# Install the 'python3-venv' package
-sudo apt install python3-venv
+# Setup the python environment and dependencies
+sudo -u mob_app_user sh -c 'cd /home/mob_app_user/ && /usr/bin/python3 -m venv .env'
+sudo -u mob_app_user bash -c 'cd /home/mob_app_user/ && source /home/mob_app_user/.env/bin/activate && pip3 install wheel uwsgi django && pip3 install -r /home/mob_app_user/Mobalysis/backend/requirements.txt && python /home/mob_app_user/Mobalysis/backend/manage.py migrate'
 
-# Create a postresql database with the name 'mobalytics'
-sudo -u postgres psql -c "CREATE DATABASE mobalytics;"
+# Get IP address
+MY_IP=`curl -s https://icanhazip.com`
 
-# Set the owner of the database to 'mob_db_user'
-sudo -u postgres psql -c "ALTER DATABASE mobalytics OWNER TO mob_db_user;"
+# Change ALLOWED_HOSTS[] in settings.py to ALLOWED_HOSTS = ['<my_ip_address>']
+sudo sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \['${MY_IP}'\]/" /home/mob_app_user/Mobalysis/backend/backend/settings.py
+
+# Set-up nginx site configuration file  
+sudo rm -f /etc/nginx/sites-available/default
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo cp /home/mob_app_user/Mobalysis/mobalysis.conf /etc/nginx/sites-available/mobalysis.conf
+sudo ln -s /etc/nginx/sites-available/mobalysis.conf /etc/nginx/sites-enabled/mobalysis.conf 
+
+sudo service nginx restart
+
+sudo mkdir -p /var/log/uwsgi/
+sudo chown mob_app_user:www-data /var/log/uwsgi/
+
+sudo mkdir -p /run/uwsgi/
+sudo chown mob_app_user:www-data /run/uwsgi/
+
+sudo -u mob_app_user bash -c 'cd /home/mob_app_user/Mobalysis/backend/'
+sudo -u mob_app_user bash -c 'uwsgi --ini /home/mob_app_user/Mobalysis/backend/uwsgi.ini'
+
+sudo chown mob_app_user:www-data /home/mob_app_user/Mobalysis/backend/mobalysis.sock
+# sudo mkdir -p /run/uwsgi/ && chown mob_app_user:www-data /run/uwsgi
